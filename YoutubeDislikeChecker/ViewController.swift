dbbef8d18e9d14019ea2e49de8292642722a478d
Elvis Cheng (ESD - Software Trainee, Digital Solutions) <elvistfcheng@esdlife.com>
2025-02-24 12:42:28 +0800

    Initial Commit

---
+++ YoutubeDislikeChecker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeDislikeChecker
+//
+//  Created by Elvis Cheng (ESD - Senior Mobile App Developer, Digital Solutions) on 24/2/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ YoutubeDislikeCheckerTests/YoutubeDislikeCheckerTests.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeDislikeCheckerTests.swift
+//  YoutubeDislikeCheckerTests
+//
+//  Created by Elvis Cheng (ESD - Senior Mobile App Developer, Digital Solutions) on 24/2/2025.
+//
+
+import Testing
+@testable import YoutubeDislikeChecker
+
+struct YoutubeDislikeCheckerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
